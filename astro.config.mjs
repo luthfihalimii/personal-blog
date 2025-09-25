3d90425ebbaa799890bef75cdce7bcdd4d7c3ed8
luthfihalimii <qlatifah62@gmail.com>
2025-09-25 12:09:25 +0700

    First Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), sitemap()],
+  adapter: cloudflare()
+});
